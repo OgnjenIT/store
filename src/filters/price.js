c42758bea85f1c9471f8c795daef889a5767ac60
OgnjenIT <reljicognjen.it@gmail.com>
2024-04-09 20:33:42 +0200

    Format price and JS for each product

--- src/filters/price.js
+++ src/filters/price.js
@@ -1,13 +1,29 @@
-import { getElement } from '../utils.js';
+import { formatPrice, getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
 const setupPrice = (store) => {
     const priceInput = getElement(".price-filter")
     const priceValue = getElement(".price-value")
-    const prices = store.map(product => {
-        return product.price
+    const prices = store.map(product => product.price)
+    const maxPrice = Math.max(...prices)
+    priceInput.max = maxPrice
+    priceInput.value = maxPrice
+    priceValue.innerHTML = `Cijena do ${formatPrice(priceInput.value)}`
+    priceInput.addEventListener("input", () => {
+        const value = priceInput.value
+        const filteredStore = store.filter(product => {
+            return product.price <= value
+        })
+        priceValue.innerHTML = `Cijena je do ${formatPrice(value)}`
+
+        if (filteredStore.length < 1) {
+            const container = getElement(".products-container")
+            container.innerHTML = `<h3 class="filter-error">Nema proizvoda do < ${formatPrice(value)} > </h3>`
+        }
+        else {
+            display(filteredStore, getElement(".products-container"))
+        }
     })
-    console.log(prices);
 };
 
 export default setupPrice;
--- src/pages/product.js
+++ src/pages/product.js
@@ -5,20 +5,44 @@ import '../cart/setupCart.js';
 // specific
 import { addToCart } from '../cart/setupCart.js';
 import { singleProductUrl, getElement, formatPrice } from '../utils.js';
+import { store } from '../store.js';
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
-// const imgDOM = getElement('.single-product-img');
-// const titleDOM = getElement('.single-product-title');
-// const companyDOM = getElement('.single-product-company');
-// const priceDOM = getElement('.single-product-price');
-// const colorsDOM = getElement('.single-product-colors');
-// const descDOM = getElement('.single-product-desc');
-// const cartBtn = getElement('.addToCartBtn');
+const loading = getElement('.page-loading');
+const centerDOM = getElement('.single-product-center');
+const pageTitleDOM = getElement('.page-hero-title');
+const imgDOM = getElement('.single-product-img');
+const titleDOM = getElement('.single-product-title');
+const companyDOM = getElement('.single-product-company');
+const priceDOM = getElement('.single-product-price');
+const colorsDOM = getElement('.single-product-colors');
+const descDOM = getElement('.single-product-desc');
+const cartBtn = getElement('.addToCartBtn');
 
 // cart product
 // let productID;
 
 // show product when page loads
+
+window.addEventListener("DOMContentLoaded", () => {
+    loading.style.display = 'none'
+    const productID = window.location.search.slice(4)
+
+    const product = store.find(product => {
+        return product.id = productID
+    })
+    const { name, company, image, price, colors } = product
+
+    document.title = `${name.toUpperCase()} | Prodavnica`
+    pageTitleDOM.innerHTML = `Proizvod | ${name}`
+    imgDOM.src = image
+    companyDOM.innerHTML = company
+    priceDOM.innerHTML = formatPrice(price)
+    titleDOM.innerHTML = name
+    colors.forEach(color => {
+        const span = document.createElement("span")
+        span.classList.add("product-color")
+        span.style.backgroundColor = color
+        colorsDOM.appendChild(span)
+    })
+})
